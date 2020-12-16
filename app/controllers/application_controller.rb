9e0fde899cce08e9a11e74df3e3ec90f36b9a010
Chase090 <carloyam21@icloud.com>
2020-12-15 20:48:16 -0800

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,13 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		#your code here!
+		user = User.new(:username => params[:username], :password => params[:password])
+		if user.save
+			redirect "/login"
+		  else
+			redirect "/failure"
+		  end
+		#* if the user has been saved, redirect to login page, if not render failure page
 	end
 
 	get "/login" do
@@ -25,7 +31,17 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		#your code here!
+		user = User.find_by(:username => params[:username])
+		#^ we need to check two conditions: first, did we find a user with that username?
+		#* In the code below, we see how we can ensure that we have a User AND that that User is authenticated. 
+		#* If the user authenticates, we'll set the session[:user_id] and redirect to the /success route. 
+		#* Otherwise, we'll redirect to the /failure route so our user can try again.
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			redirect "/success"
+		  else
+			redirect "/failure"
+		  end
 	end
 
 	get "/success" do
